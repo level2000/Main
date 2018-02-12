13f4be7023b2923a4208652a27ae33def669dc6e
Lee Angel Velez Jr <LVelez@suddath.com>
2018-02-12 17:03:33 -0500

    Fixed oauth2-oidc callback validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationComponent } from '../app/navigation/navigation.component'
+import { Router } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AutoLoginComponent } from './auto-login/auto-login.component';
+import { OidcSecurityService, AuthorizationResult } from 'angular-auth-oidc-client';
 
 
 @Component({
@@ -9,4 +13,90 @@ import { NavigationComponent } from '../app/navigation/navigation.component'
 })
 export class AppComponent {
   title = 'loading...';
+
+  constructor(public securityService: OidcSecurityService,
+    private router: Router
+) {
+    // if ( this.securityService.moduleSetup) {
+    //     this.onOidcModuleSetup();
+    // } else {
+    //     this.securityService.onModuleSetup.subscribe(() => {
+    //         this.onOidcModuleSetup();
+    //     });
+    // }
+}
+
+private onOidcModuleSetup() {
+    console.log('App.Component: onOidcModuleSetup: ' + window.location.href);
+    if ( this.gotTokenInHref() ) {
+        console.log('App.Component: onOidcModuleSetup: found token in href.');
+        this.securityService.authorizedCallback();
+    } else {
+        if ('/autologin' !== window.location.pathname) {
+            console.log('redirecting to ' + window.location.pathname + ' .');
+            this.write('redirect', window.location.pathname);
+        }
+        console.log('AppComponent:onModuleSetup');
+        this.securityService.getIsAuthorized().subscribe((authorized: boolean) => {
+            if (!authorized) {
+                console.log('redirecting to auto login.');
+                this.router.navigate(['/autologin']);
+            }
+        });
+    }
+}
+
+private onAuthorizationResultComplete(authorizationResult: AuthorizationResult) {
+    console.log('App.Component: onAuthorizationResultComplete');
+    const path = this.read('redirect');
+    console.log('AppComponent:onAuthorizationResultComplete: path: ' + path);
+    if (authorizationResult === AuthorizationResult.authorized) {
+        this.router.navigate([path]);
+    } else {
+        this.router.navigate(['/Unauthorized']);
+    }
+}
+
+private read(key: string): any {
+    const data = localStorage.getItem(key);
+    if (data != null) {
+        return JSON.parse(data);
+    }
+
+    return;
+}
+
+private write(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+ngOnInit() {
+    // if (window.location.hash) {
+    if ( this.gotTokenInHref() ) {
+            console.log('App.Component: ngOnInit: found token in href.');
+        this.securityService.authorizedCallback();
+    }
+}
+
+gotTokenInHref(): boolean {
+    return (window.location.href.indexOf('#id_token') > -1);
+}
+
+login() {
+    console.log('start login');
+    this.securityService.authorize();
+}
+
+refreshSession() {
+    console.log('start refreshSession');
+    this.securityService.authorize();
+}
+
+logout() {
+    console.log('start logoff');
+    this.securityService.logoff();
+}
+
+
+
 }
